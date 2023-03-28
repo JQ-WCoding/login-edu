e9d5e02b42d2379d9179c11ac962bf2fc0a2f49a
Jun_Q <junlee36520620@gmail.com>
2023-03-29 00:53:53 +0900

    파일 업로드 기능 세팅

--- src/main/java/com/example/loginsample/domain/User.java
+++ src/main/java/com/example/loginsample/domain/User.java
@@ -13,12 +13,17 @@ import lombok.*;
 public class User {
     public User() {
     }
+
     @Id
     @GeneratedValue ( strategy = GenerationType.IDENTITY )
     private Long id;
+
+    @Column ( name = "USER_NAME" )
     private String userName;
 
+    @Column ( name = "USER_ID" )
     private String userId;
 
+    @Column ( name = "USER_PASSWORD" )
     private String userPassword;
 }
